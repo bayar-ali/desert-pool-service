0d41a7d45898235dfa4c311c1061417bb06ddfbf
Dazacher <dazacher2000@yahoo.com>
2020-07-29 15:12:49 -0700

    Got logo working.

--- client/src/components/navbar/NavBar.js
+++ client/src/components/navbar/NavBar.js
@@ -4,19 +4,20 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 import Form from "react-bootstrap/Form";
 import FormControl from "react-bootstrap/FormControl";
 import Button from "react-bootstrap/Button";
-import { Nav } from "react-bootstrap";
+import { Nav, Image } from "react-bootstrap";
 // import MenuItem from "react-bootstrap/MenuItem";
 import { NavLink } from "react-router-dom";
 import "./NavBar.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import PoolServiceLogo from "../../assets/images/Swimming-pool-service-aqua-logo-580x348.jpg";
 
 const NavbarRx = () => {
     return (
-        <Navbar bg="light" expand="lg">
+        <Navbar className="navBar-font" bg="light" expand="lg">
             <Navbar bg="dark">
                 <Navbar.Brand href="/">
-                    <img
-                        src="../../Swimming-pool-service-aqua-logo-580x348.jpg"
+                    <Image
+                        src={PoolServiceLogo}
                         width="30"
                         height="30"
                         className="d-inline-block align-top"
@@ -28,19 +29,19 @@ const NavbarRx = () => {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav>
-                    <NavLink to="/">Login</NavLink>
+                    <Nav.Link href="/login">Login</Nav.Link>
                     <NavDropdown title="Customers" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#/customers">Customers</NavDropdown.Item>
-                        {/* <NavDropdown.Item href="/viewcustomer">View Customer</NavDropdown.Item> */}
+                        <NavDropdown.Item href="/customer">Customers</NavDropdown.Item>
+                        <NavDropdown.Item href="/viewcustomer">View Customer</NavDropdown.Item>
                         {/* <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
                         <NavDropdown.Divider />
                         <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item> */}
                     </NavDropdown>
                 </Nav>
-                <Form inline>
+                {/* <Form inline>
                     <FormControl type="text" placeholder="Search" className="mr-sm-2" />
                     <Button variant="outline-success">Search</Button>
-                </Form>
+                </Form> */}
             </Navbar.Collapse>
         </Navbar>
     );
