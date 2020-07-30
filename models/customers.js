4508338e5f1ccf9b37878eea1089bf648e4ce402
Denzell Grant <denzgrant@gmail.com>
2020-07-29 22:00:39 -0700

    added auth0, work orders, and connected to mongoAtlas

--- models/customers.js
+++ models/customers.js
@@ -2,13 +2,27 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const custSchema = new Schema({
-  Name: { type: String, required: true, minlength: 1 },
-  Address: { type: String, required: true },
-  Email: { type: String, required: true },
-  Salt_Pool: { type: Boolean, default: false, required: true }, 
-  Filter: String,
-  create_date: { type: Date, default: Date.now() }, 
-  scheduled_date: { type: Date, default: Date.now() }
+  Name: { 
+    type: String, 
+    required: true, 
+    minlength: 1 
+  },
+  Address: { 
+    type: String, 
+    required: true 
+  },
+  Email: { 
+    type: String, 
+    required: true 
+  },
+  create_date: { 
+    type: Date, 
+    default: Date.now() 
+  },
+  work_orders: [{
+    type: Schema.Types.ObjectId,
+    ref: "workOrder"
+  }]
 
 });
 
--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
+var jwt = require('express-jwt');
+var jwks = require('jwks-rsa');
 //For test
 const Customers = mongoose.model("Customers")
 const PORT = process.env.PORT || 3001;
+
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -17,34 +20,62 @@ if (process.env.NODE_ENV === "production") {
 // Add routes, both API and view
 app.use(routes);
 
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/customerlist", { useNewUrlParser: true, useUnifiedTopology: true });
+const uri  = (`mongodb+srv://dgrant:desertpools@desertpools.gd8gi.mongodb.net/pools_db?retryWrites=true&w=majority`)
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log('MongoDB Connected')
+})
+.catch(err => console.log(err))
 
 // Start the API server
 app.listen(PORT, function () {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
 
+// Auth0 
+var jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+      cache: true,
+      rateLimit: true,
+      jwksRequestsPerMinute: 5,
+      jwksUri: 'https://dev-4anh9rsd.us.auth0.com/.well-known/jwks.json'
+}),
+audience: 'https://desertpools-users/api',
+issuer: 'https://dev-4anh9rsd.us.auth0.com/',
+algorithms: ['RS256']
+});
+
+
+app.use(jwtCheck);
+
+app.get('/authorized', function (req, res) {
+  res.send('Secured Resource');
+});
+
 //test seed 
-const createCustomer = async () => {
-  try {
-    const customer = {
-      Name: "Bob Smith",
-      Address: "123 Smith Lane",
-      Email: "bsmith@gmail.com",
-      Salt_Pool: true,
-      Filter: "Sand",
-      created_date: "7/26/20",
-      scheduled_date: "7/29/20"
-    }
+// const createCustomer = async () => {
+//   try {
+//     const customer = {
+//       Name: "Bob Smith",
+//       Address: "123 Smith Lane",
+//       Email: "bsmith@gmail.com",
+//       Salt_Pool: true,
+//       Filter: "Sand",
+//       created_date: "7/26/20",
+//       scheduled_date: "7/29/20"
+//     }
 
-    const data = await Customers.create(customer);
+//     const data = await Customers.create(customer);
 
-    console.log(data);
+//     console.log(data);
 
-  } catch (error) {
-    console.log(error)
-  }
-}
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
 
 createCustomer(); 
