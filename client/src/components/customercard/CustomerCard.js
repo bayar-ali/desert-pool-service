b819907ce3565aa263825249638675491a1a5033
Dazacher <dazacher2000@yahoo.com>
2020-08-06 16:04:28 -0700

    Updated customer data pulled from db to have address as an object.

--- client/src/components/customercard/CustomerCard.js
+++ client/src/components/customercard/CustomerCard.js
@@ -3,9 +3,9 @@ import "./CustomerCard.css"
 import { MDBRow, MDBCol, MDBBtn, MDBInput } from 'mdbreact';
 // import "./CustomerCard.css"
 
-function CustomerCard({CustomerRecord, handleChange, editable}) {
+function CustomerCard({ CustomerRecord, handleChange, editable }) {
 
-   
+
     return (
         <>
             <form className="formatForm">
@@ -13,9 +13,9 @@ function CustomerCard({CustomerRecord, handleChange, editable}) {
                     <MDBCol>
                         <MDBInput
                             className="formatInput"
-                            label='Customer Name'
-                            name="Name"
-                            value={CustomerRecord.Name}
+                            label='First Name'
+                            name="firstName"
+                            value={CustomerRecord.firstName}
                             onChange={handleChange}
                             icon='user'
                             group
@@ -24,78 +24,99 @@ function CustomerCard({CustomerRecord, handleChange, editable}) {
                             validate
                             error='wrong'
                             success='right'
-                            placeHolder="Enter Customer Name"
+                            placeHolder="Enter Customer First Name"
                         />
-                        <MDBRow>
-                            <MDBCol lg="6">
-                                <MDBInput
-                                    className="formatInput"
-                                    label='Customer Address'
-                                    name="Address"
-                                    value={CustomerRecord.Address}
-                                    onChange={handleChange}
-                                    icon='address-book'
-                                    group
-                                    type='text'
-                                    validate
-                                    error='wrong'
-                                    success='right'
-                                    placeHolder="Enter Customer Address"
-                                />
-                            </MDBCol>
-                            <MDBCol lg="3">
-                                <MDBInput
-                                    className="formatInput"
-                                    label='Customer City'
-                                    name="City"
-                                    value={CustomerRecord.City}
-                                    onChange={handleChange}
-                                    icon='city'
-                                    group
-                                    type='text'
-                                    validate
-                                    error='wrong'
-                                    success='right'
-                                    placeHolder="Enter City"
-                                />
-                            </MDBCol>
-                            <MDBCol lg="1">
-                                <MDBInput
-                                    className="formatInput"
-                                    label='Customer State'
-                                    name="State"
-                                    value={CustomerRecord.State}
-                                    onChange={handleChange}
-                                    icon='flag-usa'
-                                    group
-                                    type='text'
-                                    validate
-                                    error='wrong'
-                                    success='right'
-                                    placeHolder="State"
-                                />
-                            </MDBCol>
-                            <MDBCol lg="2">
-                                <MDBInput
-                                    className="formatInput"
-                                    label='Customer Zip Code'
-                                    name="Zip"
-                                    value={CustomerRecord.Zip}
-                                    onChange={handleChange}
-                                    icon='plane'
-                                    group
-                                    type='text'
-                                    validate
-                                    error='wrong'
-                                    success='right'
-                                    placeHolder="Enter Zop Code"
-                                />
-                            </MDBCol>
-                        </MDBRow>
+                    </MDBCol>
+                    <MDBCol>
                         <MDBInput
                             className="formatInput"
-                            label='Customer Email Address'
-                            name="Email"
+                            label='Last Name'
+                            name="lastName"
+                            value={CustomerRecord.lastName}
+                            onChange={handleChange}
+                            icon='user'
+                            group
+                            disabled={editable}
+                            type='text'
+                            validate
+                            error='wrong'
+                            success='right'
+                            placeHolder="Enter Customer Last Name"
+                        />
+                    </MDBCol>
+                </MDBRow>
+                <MDBRow>
+                    <MDBCol lg="6">
+                        <MDBInput
+                            className="formatInput"
+                            label='Street Address'
+                            name="address"
+                            value={CustomerRecord.address}
+                            onChange={handleChange}
+                            icon='address-book'
+                            group
+                            type='text'
+                            validate
+                            error='wrong'
+                            success='right'
+                            placeHolder="Enter Customer Address"
+                        />
+                    </MDBCol>
+                    <MDBCol lg="3">
+                        <MDBInput
+                            className="formatInput"
+                            label='City'
+                            name="city"
+                            value={CustomerRecord.city}
+                            onChange={handleChange}
+                            icon='city'
+                            group
+                            type='text'
+                            validate
+                            error='wrong'
+                            success='right'
+                            placeHolder="Enter City"
+                        />
+                    </MDBCol>
+                    <MDBCol lg="1">
+                        <MDBInput
+                            className="formatInput"
+                            label='State'
+                            name="state"
+                            value={CustomerRecord.state}
+                            onChange={handleChange}
+                            icon='flag-usa'
+                            group
+                            type='text'
+                            validate
+                            error='wrong'
+                            success='right'
+                            placeHolder="State"
+                        />
+                    </MDBCol>
+                    <MDBCol lg="2">
+                        <MDBInput
+                            className="formatInput"
+                            label='Zip Code'
+                            name="zipcode"
+                            value={CustomerRecord.zipcode}
+                            onChange={handleChange}
+                            icon='plane'
+                            group
+                            type='text'
+                            validate
+                            error='wrong'
+                            success='right'
+                            placeHolder="Zip Code"
+                        />
+                    </MDBCol>
+                </MDBRow>
+                <MDBRow>
+                    <MDBCol>
+                        <MDBInput
+                            className="formatInput"
+                            label='Email Address'
+                            name="email"
                             value={CustomerRecord.Email}
                             onChange={handleChange}
                             icon='envelope-open-text'
@@ -108,8 +129,8 @@ function CustomerCard({CustomerRecord, handleChange, editable}) {
                         />
                         <MDBInput
                             className="formatInput"
-                            label='Customer Phone Number'
-                            name="Phone_Num"
+                            label='Phone Number'
+                            name="phone_num"
                             value={CustomerRecord.Phone_Num}
                             onChange={handleChange}
                             icon='phone'
@@ -122,7 +143,7 @@ function CustomerCard({CustomerRecord, handleChange, editable}) {
                         />
                     </MDBCol>
                 </MDBRow>
-            </form>
+            </form >
         </>
     )
 }
