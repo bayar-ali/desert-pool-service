be3fafd96653f7bd43d3825c9e63bc4a9e7ad023
Denzell Grant <denzgrant@gmail.com>
2020-08-11 07:32:54 -0700

    pushing to heroku

--- server.js
+++ server.js
@@ -15,6 +15,9 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 // Add routes, both API and view
 app.use(routes);
 
