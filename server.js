b4361d1f075afeb6bc2644b5c3eb26c52c8a8c9e
Denzell Grant <denzgrant@gmail.com>
2020-08-13 20:20:34 -0700

    fixed bugs

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || '3001';
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 // Add routes, both API and view
 app.use(routes);
